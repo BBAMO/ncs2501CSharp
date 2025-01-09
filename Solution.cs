9c0fb5f85ea922d4c68bb6de7344a8e2b5d1e974
BBAMO <gao0708@naver.com>
2025-01-09 15:50:08 +0900

    Update Solution.cs

--- Solution.cs
+++ Solution.cs
@@ -1,5 +1,11 @@
 class Solution
 {
+    /// <summary>
+    /// 두 수의 차
+    /// </summary>
+    /// <param name="num1">인자1</param>
+    /// <param name="num2">인자2</param>
+    /// <returns>반환값</returns>
     public int solution0109(int num1, int num2)
     {
         // 대안 2
